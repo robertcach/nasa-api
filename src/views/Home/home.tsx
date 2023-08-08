49519be2e8a8006bc1b4d307e173ac85db9ee932
Robert Cabrera <cabrera.rch@gmail.com>
2023-08-08 17:38:06 +0200

    feat: create Home view within Header component

---
+++ src/views/Home/home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Header } from "../../components/Header";
+
+export function Home() {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+}
