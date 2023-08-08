f0c4999ed12ceb7ff5bb89a2ca72e5309450117b
Robert Cabrera <cabrera.rch@gmail.com>
2023-08-08 17:38:42 +0200

    feat: update App.tsx with Home View and background styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, View, Platform } from "react-native";
+import { Home } from "./src/views/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ paddingTop: Platform.OS === "android" ? 30 : 0 }}>
+        <Home />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgba(7, 26, 93, 155)",
   },
 });
