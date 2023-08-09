6e671fcdf82d7cba88ac27346cc4f894d3460fc6
Robert Cabrera <cabrera.rch@gmail.com>
2023-08-09 18:39:54 +0200

    feat: make fetch from Home and send data to TodaysImage component

--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View, Button } from "react-native";
 import { Header } from "../../components/Header";
-
+import { useImage } from "../../hooks/useImage";
+import { TodayImage } from "../../types";
+import TodaysImage from "../../components/TodaysImage/todaysImage";
 export function Home() {
+  const [todayImage, setTodayImage] = useState<TodayImage | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const image: TodayImage = await useImage();
+      setTodayImage(image);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
+      {todayImage && <TodaysImage {...todayImage} />}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+  },
+});
