2ba0ac13d0cad96f86cb9b1f1e74b3b715da94d4
Robert Cabrera <cabrera.rch@gmail.com>
2023-08-12 13:29:37 +0200

    feat: create logic to fetch last five day's images and set response in a state

--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Header } from "../../components/Header";
 import { useImage } from "../../hooks/useImage";
 import { TodayImage } from "../../types";
 import TodaysImage from "../../components/TodaysImage/todaysImage";
+import { format, subDays } from "date-fns";
+import LastFiveDaysImages from "../../components/LastFiveDaysImages/lastFiveDaysImages";
 export function Home() {
   const [todayImage, setTodayImage] = useState<TodayImage | undefined>(
     undefined
   );
+  const [lastFiveDaysImages, setLastDaysImages] = useState<TodayImage[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,7 +18,19 @@ export function Home() {
       setTodayImage(image);
     };
 
+    const loadLast5DaysImages = async () => {
+      const todayDate = format(new Date(), "yyyy-MM-dd");
+      const fiveDaysAgo = format(subDays(new Date(), 5), "yyyy-MM-dd");
+
+      const fetchLastFiveDaysImages: TodayImage[] = await useImage(
+        `&start-date=${fiveDaysAgo}&end_date=${todayDate}`
+      );
+
+      setLastDaysImages(fetchLastFiveDaysImages);
+    };
+
     fetchData();
+    loadLast5DaysImages();
   }, []);
 
   return (
